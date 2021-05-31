81e86c870122979748b67544db98841c2f4f325e
stan-28 <b.luca999@gmail.com>
2021-05-31 16:06:15 +0200

    +-

--- js/vasarlas.js
+++ js/vasarlas.js
@@ -1,29 +1,50 @@
-window.addEventListener("load", init, false);
-
-function $(id) {
-    return document.getElementById(id);
+//forrás: https://codepen.io/baselakasha/pen/RZybYe
+window.onload=function(){
+    var minusBtn = document.getElementById("minus"),
+        plusBtn = document.getElementById("plus"),
+        numberPlace = document.getElementById("szam"),
+        submitBtn = document.getElementById("kosarbaTesz"),
+        number = 0, /// number value
+        min = 0, /// min number
+        max = 30; /// max number
+        
+    minusBtn.onclick = function(){
+        if (number>min){
+           number--; /// Minus 1 of the number
+           numberPlace.innerText = number ; /// Display the value in place of the number
+           
+        }
+        if(number == min) {        
+            numberPlace.style.color= "red";
+            setTimeout(function(){numberPlace.style.color= "black"},500)
+        }
+        else {
+          numberPlace.style.color="black";            
+           }
+                
+    }
+    plusBtn.onclick = function(){
+        if(number<max){
+           number++;
+           numberPlace.innerText = number ; /// Display the value in place of the number
+        }     
+        if(number == max){
+               numberPlace.style.color= "red";
+               setTimeout(function(){numberPlace.style.color= "black"},500)
+        }
+        else {
+               numberPlace.style.color= "black";
+               
+        }
+    }
+    submitBtn.onclick = function(){
+        alert("you choice : " + number);
+    }
+    
 }
 
-/*
-function minusz(){
-    let szam = $("szam").value;
-    if($('minusz').clicked == true){
-        szam--;
-    }
+let kosar = [];
 
+function vasarlas(){
+    
 }
-
-function plusz(){
-    if($("plusz").clicked == true){
-        szam++;
-    }
-
-}*/
-
-function increment() {
-    $('szam').stepUp();
- }
-
- function decrement() {
-    $('szam').stepDown();
- }
