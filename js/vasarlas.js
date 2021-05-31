bf9336fbbbd0acf78413a6cab8a1c9ef1987f2c9
stan-28 <b.luca999@gmail.com>
2021-05-31 19:08:44 +0200

    Update vasarlas.js

--- js/vasarlas.js
+++ js/vasarlas.js
@@ -68,17 +68,6 @@ window.onload=function(){
 //setting default attribute to disabled of minus button
 document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
 
-//taking value to increment decrement input value
-var valueCount
-
-//taking price value in variable
-var price = document.getElementById("price").innerText;
-
-//price calculation function
-function priceTotal() {
-    var total = valueCount * price;
-    document.getElementById("price").innerText = total
-}
 
 //plus button
 document.querySelector(".plus-btn").addEventListener("click", function () {
@@ -96,8 +85,6 @@ document.querySelector(".plus-btn").addEventListener("click", function () {
         document.querySelector(".minus-btn").classList.remove("disabled")
     }
 
-    //calling price function
-    priceTotal()
 })
 
 //plus button
@@ -115,6 +102,6 @@ document.querySelector(".minus-btn").addEventListener("click", function () {
         document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
     }
 
-    //calling price function
-    priceTotal()
 })
+
+
