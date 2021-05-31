eb57048169fd82de0facdd61f5ccddd775e474bd
stan-28 <b.luca999@gmail.com>
2021-05-31 18:48:15 +0200

    btn

--- js/vasarlas.js
+++ js/vasarlas.js
@@ -1,11 +1,10 @@
-
 window.addEventListener("load", init, false);
 
-function $(id){
+function $(id) {
     return document.getElementById(id);
 }
 
-function init(){
+function init() {
     $("feliratkozas").addEventListener("click", email, false);
 }
 /*
@@ -17,7 +16,7 @@ let email = function() {
 let email = function () {
     document.getElementById('email').value = '';
 }
-
+/*
 //forrás: https://codepen.io/baselakasha/pen/RZybYe
 window.onload=function(){
     var minusBtn = document.getElementById("minus"),
@@ -62,7 +61,60 @@ window.onload=function(){
     }
     
 }
+*/
 
-//let kosar = [];
 
+// forrás: https://www.youtube.com/watch?v=GXsWa_6vnOo&t=306s
+//setting default attribute to disabled of minus button
+document.querySelector(".minus-btn").setAttribute("disabled", "disabled");
 
+//taking value to increment decrement input value
+var valueCount
+
+//taking price value in variable
+var price = document.getElementById("price").innerText;
+
+//price calculation function
+function priceTotal() {
+    var total = valueCount * price;
+    document.getElementById("price").innerText = total
+}
+
+//plus button
+document.querySelector(".plus-btn").addEventListener("click", function () {
+    //getting value of input
+    valueCount = document.getElementById("quantity").value;
+
+    //input value increment by 1
+    valueCount++;
+
+    //setting increment input value
+    document.getElementById("quantity").value = valueCount;
+
+    if (valueCount > 1) {
+        document.querySelector(".minus-btn").removeAttribute("disabled");
+        document.querySelector(".minus-btn").classList.remove("disabled")
+    }
+
+    //calling price function
+    priceTotal()
+})
+
+//plus button
+document.querySelector(".minus-btn").addEventListener("click", function () {
+    //getting value of input
+    valueCount = document.getElementById("quantity").value;
+
+    //input value increment by 1
+    valueCount--;
+
+    //setting increment input value
+    document.getElementById("quantity").value = valueCount
+
+    if (valueCount == 1) {
+        document.querySelector(".minus-btn").setAttribute("disabled", "disabled")
+    }
+
+    //calling price function
+    priceTotal()
+})
