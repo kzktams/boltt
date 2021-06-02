b294e5222339684ef7a9455e652e39d48e7a2d70
stan-28 <b.luca999@gmail.com>
2021-06-02 17:07:45 +0200

    console

--- js/vasarlas.js
+++ js/vasarlas.js
@@ -58,4 +58,88 @@ function kosarbaTesz() {
 }
 */
 
+//kosár
+/*
+function kosar() {
+  let termekek = [];
+  let termekNev = document.getElementById('termek').value;
+  let ar = document.getElementById("ar").value;
+  let mennyiseg = document.getElementById('.field').value
+  var mennyiseg = $('.field', this.parentNode)[0];
 
+  termekek.push(termekNev);
+}*/
+
+/*
+let termek = {
+  termekek: [],
+  termeknev: document.getElementById('termek').value, 
+  ar: document.getElementById("ar").value, 
+  mennyiseg: document.getElementById('.field').value
+};
+
+//termékek 
+var products = {
+  123: {
+    name : "asahi",
+    img : "asahi.jpg",
+    price : 2034
+  },
+  124: {
+    name : "haineken",
+    img : "dummy-pdt-b.jpg",
+    price : 1247
+  },
+  125: {
+    name : "unicum",
+    img : "dummy-pdt-a.jpg",
+    price : 675
+  },
+  126: {
+    name : "jager",
+    img : "dummy-pdt-a.jpg",
+    price : 842
+  }
+};
+
+$('.kosar').click(function() {
+  //let termekek = [];
+  //let termekNev = document.getElementById('termek').value;
+  //let ar = document.getElementById("ar").value;
+  //let mennyiseg = document.getElementById('.field').value;
+
+  /*
+  let termek = {
+    termekek: [],
+    termeknev: document.getElementById('termek').value, 
+    ar: document.getElementById("ar").value, 
+    //mennyiseg: document.getElementById('.field').value
+  };
+
+  var termekek= [];
+  var kosar = $('.field', this.parentNode)[0];
+  termekek.push(products);
+  console.log("lefut");
+  console.log(products.name);
+});
+*/
+
+$('.submitbutton').click(function() {
+var termekek = [];
+var termekekNeve = [];
+var osszAr = [];
+var ar = $('#item1 span').text();
+var nev = $('#itemNev span').text();
+var mennyiseg = $("input:text").val();
+
+var osszeg = ar*mennyiseg;
+ 
+termekek.push(ar);
+termekekNeve.push(nev);
+osszAr.push(osszeg);
+
+console.log(termekek);
+console.log(termekekNeve);
+console.log(mennyiseg);
+console.log(osszAr);
+});
