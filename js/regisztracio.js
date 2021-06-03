c5647668b692543a5dc2f9cb247a4346e4b7b0eb
stan-28 <b.luca999@gmail.com>
2021-06-03 18:31:46 +0200

    regisztracio

--- js/regisztracio.js
+++ js/regisztracio.js
@@ -7,15 +7,44 @@ function $(id) {
 function init() {
     $("gomb").addEventListener("click", ures, false);
 
+    //$("gomb").addEventListener("click", elvisz, false);
+
 }
 
 let ures = function () {
     let email = $("bejelentkezE").value;
     let jelszo = $("jelszo").value;
     let jelszoKetto = $("jelszo2").value;
+    //let elerheto = $("oldalLink").disabled = true;
 
-    if(email.length < 1 || jelszo.length<1 || jelszoKetto.length < 1){
-        console.log("igen");
-        $("bejelentkezE").style.color = "red";
+/* 
+        if ($("bejelentkezE").value < 1) {
+            $("ures1").innerHTML = "Ne hagyjon üres mezőket!";
+        }
+        if ($("jelszo").value < 1) {
+            $("ures2").innerHTML = "Ne hagyjon üres mezőket!";
+        }
+        if ($("jelszo2").value < 1) {
+            $("ures3").innerHTML = "Ne hagyjon üres mezőket!";
+        }
+        if ($("jelszo").value != $("jelszo2").value) {
+            $("ures3").innerHTML = "A jelszó  nem egyezik";
+        }
+
+        $("gomb").disabled = false;*/
+
+        if($("bejelentkezE").value < 1 && $("jelszo").value < 1 && $("jelszo2").value < 1){
+            $("ures3").innerHTML = "Ne hagyjon üres mezőket!";
+        }else{
+            $("oldalLink").disabled = false;
+        }
+    
+}
+
+/*
+let elvisz = function () {
+    if(!ures()){
+
     }
 }
+*/
